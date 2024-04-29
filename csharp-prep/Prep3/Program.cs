62732c8354abcd603ef6e44c02d7334b0031853a
MariaJulia13 <mariajuliadrummond13@gmail.com>
2024-04-29 11:47:32 +0100

    Added Comment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,58 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        // Core Requirement: Ask the user for the magic number.
+        Console.Write("Enter the magic number: ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        string playAgainResponse;
+
+        // Stretch Challenge: Loop for playing again
+        do
+        {
+            // Initialize variables for tracking guesses
+            int numberOfGuesses = 0;
+            int guess = 0;
+
+            // Core Requirement: Loop until the guess matches the magic number
+            do
+            {
+                // Core Requirement: Ask the user for a guess.
+                Console.Write("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                // Determine if the guess is higher, lower, or correct
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            while (guess != magicNumber);
+
+            // Stretch Challenge: Inform the user of the number of guesses
+            Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+
+            // Core Requirement: Ask the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgainResponse = Console.ReadLine();
+
+            // Stretch Challenge: Loop back and play again if the user wants to continue
+        }
+        while (playAgainResponse.ToLower() == "yes");
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
