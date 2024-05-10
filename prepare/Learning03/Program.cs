35b2ad2a45d790b40ef4c1da97d64a91621d5b5b
MariaJulia13 <mariajuliadrummond13@gmail.com>
2024-05-10 11:43:41 +0100

    Added comment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Create instances using different constructors
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        // Output representations of fractions
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        // Test getters and setters - not applicable since the provided class doesn't have them
+
+        Console.ReadLine();
     }
 }
